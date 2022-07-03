10cfd523d4b9bf9c952e7b4e22e89e0c914c44b1
Richard Walsh <richwalsh2@googlemail.com>
2022-07-03 17:27:51 +0100

    section 8 lesson 65

---
+++ includes/html/navigation.php
@@ -0,0 +1,17 @@
+<nav class="nav-bar">
+    <ul>
+        <li class="nav-logo"><a href="/"><ion-icon name="radio-sharp"></ion-icon></a></li>
+        <li class="nav-search-form">
+            <form id="nav-form" action="index.php" method="post">
+                <div class="nav-search-form__container">
+                    <label for="nav-search"></label>
+                    <input type="text" placeholder="Search" name="nav-search">
+                    <button type="submit" id="nav-search__btn"><span class="nav-search-form__icon"><ion-icon name="search-outline"></ion-icon></span></button>
+                </div>
+            </form>
+        </li>
+        <li class="nav-link"><a href="/">browse</a></li>
+        <li class="nav-link"><a href="/">your music</a></li>
+        <li class="nav-link"><a href="/">bunk moreland</a></li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -25,63 +25,24 @@ if(isset($_SESSION['userLoggedIn'])) {
 <body>
     <main class="main">
         <section class="navigation">
-            <nav class="nav-bar">
-                <ul>
-                    <li class="nav-logo"><a href="/"><ion-icon name="radio-sharp"></ion-icon></a></li>
-                    <li class="nav-search-form">
-                        <form id="nav-form" action="index.php" method="post">
-                            <div class="nav-search-form__container">
-                                <label for="nav-search"></label>
-                                <input type="text" placeholder="Search" name="nav-search">
-                                <button type="submit" id="nav-search__btn"><span class="nav-search-form__icon"><ion-icon name="search-outline"></ion-icon></span></button>
-                            </div>
-                        </form>
-                    </li>
-                    <li class="nav-link"><a href="/">browse</a></li>
-                    <li class="nav-link"><a href="/">your music</a></li>
-                    <li class="nav-link"><a href="/">bunk moreland</a></li>
-                </ul>
-            </nav>
+            <?php require('includes/html/navigation.php') ?>
         </section>
-        <section class="main-content"></section>
+        <section class="main-content">
+            <h1 class="heading__primary main-heading">You Might Also Like</h1>
+
+            <?php 
+                $albumQuery = mysqli_query($dBConnection, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+                
+                while($dbRow = mysqli_fetch_array($albumQuery)) {
+                    echo "<div class='album-card'>
+                            <img class='album-image' src='" . $dbRow['artworkPath'] . "'>
+                            <p class='album-title'>" . $dbRow['title'] . "</p>
+                        </div>";
+                }
+            ?>
+        </section>
         <section class="now-playing-bar-container">
-            <div class="now-playing-bar">
-                <div class="now-playing-bar__left">
-                    <div class="now-playing__img">
-                        <img src="./assets//images/now_playing.jpg" alt="now playing">
-                    </div>
-                    <div class="now-playing__info">
-                        <p class="now-playing__song-title">i want to be adored</p>
-                        <p class="now-playing__song-artist">the stone roses</p>
-                    </div>
-                </div>
-                <div class="now-playing-bar__center">
-                    <div class="now-playing-bar__controls">
-                        <button class="now-playing-bar__button" title="Shuffle"><ion-icon name="shuffle-outline"></ion-icon></ion-icon></button>
-                        <button class="now-playing-bar__button" title="Skip back"><ion-icon name="play-skip-back"></ion-icon></button>
-                        <button class="now-playing-bar__button button-play" title="Play"><ion-icon name="play-circle-outline"></ion-icon></button>
-                        <button class="now-playing-bar__button" title="Skip forward"><ion-icon name="play-skip-forward"></ion-icon></button>
-                        <button class="now-playing-bar__button" title="Repeat"><ion-icon name="repeat-outline"></ion-icon></button>
-                    </div>
-                    <div class="now-playing-bar__progress-bar">
-                        <span class="progress-time current">0.00</span>
-                        <div class="progress-bar">
-                            <div class="progress-bar__background">
-                                <div class="progress-bar__foreground"></div>
-                            </div>
-                        </div>
-                        <span class="progress-time remaining">0.00</span>
-                    </div>
-                </div>
-                <div class="now-playing-bar__right">
-                    <button class="now-playing-bar__button"><ion-icon name="volume-medium-outline"></ion-icon></button>
-                    <div class="now-playing__volume-bar">
-                        <div class="volume-bar__background">
-                            <div class="volume-bar__foreground"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php require('includes/html/nowPlayingBar.php') ?>
         </section>
     </main>
     
